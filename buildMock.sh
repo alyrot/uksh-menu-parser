ce4ac9c10f39952a9984fa8f87fc4494bbeaa1c6
alyrot <alyrot@protonmail.com>
2020-11-23 19:41:55 +0100

    updated buildMock.sh

--- buildMock.sh
+++ buildMock.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [[ -z "${GOPATH}" ]]; then
+MOCKGEN=mockgen
+else
+MOCKGEN=$GOPATH/bin/mockgen
+fi
 mkdir -p mocks/cmd/web/
-$GOPATH/bin/mockgen -source cmd/web/menuCache.go -destination mocks/cmd/web/menuCache.go -package menuCacheMock
+echo "Using $MOCKGEN"
+$MOCKGEN -source cmd/web/menuCache.go -destination mocks/cmd/web/menuCache.go -package menuCacheMock
 
 mkdir -p mocks/pkg/parser
-$GOPATH/bin/mockgen -source pkg/parser/parser.go -destination mocks/pkg/parser/parser.go -package parserMock
+$MOCKGEN -source pkg/parser/parser.go -destination mocks/pkg/parser/parser.go -package parserMock
