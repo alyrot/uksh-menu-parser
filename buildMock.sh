0ce444774649786fbcd098d1ef629d8045df476f
alyrot <alyrot@protonmail.com>
2020-11-23 20:53:12 +0100

    updated buildMock.sh

--- buildMock.sh
+++ buildMock.sh
@@ -7,8 +7,8 @@ MOCKGEN=$GOPATH/bin/mockgen
 fi
 mkdir -p mocks/cmd/web/
 echo "Using $MOCKGEN"
-$MOCKGEN -source cmd/web/menuCache.go -destination mocks/cmd/web/menuCache.go -package menuCacheMock
+$MOCKGEN -source cmd/web/menuCache.go -destination mocks/cmd/web/menuCache.go -package menuCacheMock -self_package github.com/alyrot/menuToText/mocks/cmd/web
 
 mkdir -p mocks/pkg/parser
-$MOCKGEN -source pkg/parser/parser.go -destination mocks/pkg/parser/parser.go -package parserMock
+$MOCKGEN -source pkg/parser/parser.go -destination mocks/pkg/parser/parser.go -package parserMock -self_package github.com/alyrot/menuToText/mocks/pkg/parser
 echo "Done generating mocks"
